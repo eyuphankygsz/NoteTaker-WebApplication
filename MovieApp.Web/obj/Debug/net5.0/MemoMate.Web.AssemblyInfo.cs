2438718a6d5f47bc212b96101b3071db347426db
Eyüphan Kaygusuz <eyuphankaygusuz@gmail.com>
2024-06-24 01:16:23 +0300

    additions

--- MovieApp.Web/obj/Debug/net5.0/MemoMate.Web.AssemblyInfo.cs
+++ MovieApp.Web/obj/Debug/net5.0/MemoMate.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MemoMate.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f274d4f1c5416cb3299226537b2c6d87fc34cebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdfc8cbd062c2f46c64c85925542a70519e95a82")]
 [assembly: System.Reflection.AssemblyProductAttribute("MemoMate.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MemoMate.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
